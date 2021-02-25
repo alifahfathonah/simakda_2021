386278d46fedbc85dd3e40bea70a47ec7588ea15
Alamul huda <alamulhuda.tsi@gmail.com>
2021-02-25 10:40:13 +0700

    Realisasi Pendapatan
    Menu Penatausahaan->Bendahara Umum Daerah->Laporan Bendahara Umum Daerah->Realisasi Pendapatan
    Tambahkan tabel : trskpd_pend
    Function di Database : penerimaan_kasda_unit, penerimaan_kasda

--- application/controllers/lamp_pmk.php
+++ application/controllers/lamp_pmk.php
@@ -161,7 +161,7 @@ class Lamp_pmk extends CI_Controller
 		$jabatan='';
 		$nip='';
 	}else{
-	$ttd=str_replace("123456789"," ",$ttd);
+	$ttd=str_replace("abc"," ",$ttd);
 	//echo $ttd;
 	//$sqlsc="SELECT nama,jabatan,pangkat FROM ms_ttd where nip='$ttd' AND kode='KD'";
 	$sqlsc="SELECT TOP 1 nama as nm,nip as nip,jabatan as jab,pangkat FROM ms_ttd where kode='BUD' and nip='$ttd'";
@@ -5697,5 +5697,280 @@ class Lamp_pmk extends CI_Controller
 		$this->template->set('title', 'REKAP PENGELUARAN');   
 		$this->template->load('template','tukd/sp2d/daftar_pengeluaran_rekap_kasda',$data) ; 
 	}
+ 
+	function cetak_real_pend_skpd($bulan='',$ctk='',$anggaran='',$kd_skpd='',$jenis='',$tglttd='',$ttd=''){
+        $lntahunang = $this->session->userdata('pcThang');       
+		switch  ($bulan){
+        case  1:
+        $judul="JANUARI";
+        break;
+        case  2:
+        $judul="FEBRUARI";
+        break;
+        case  3:
+        $judul= "MARET";
+        break;
+        case  4:
+        $judul="APRIL";
+        break;
+        case  5:
+        $judul= "MEI";
+        break;
+        case  6:
+        $judul= "JUNI";
+        break;
+        case  7:
+        $judul= "JULI";
+        break;
+        case  8:
+        $judul= "AGUSTUS";
+        break;
+        case  9:
+        $judul= "SEPTEMBER";
+        break;
+        case  10:
+        $judul= "OKTOBER";
+        break;
+        case  11:
+        $judul= "NOVEMBER";
+        break;
+        case  12:
+        $judul= "DESEMBER";
+        break;
+    }
+	$tanggal = $tglttd == '-' ? '' : 'Pontianak, '.$this->tukd_model->tanggal_format_indonesia($tglttd) ;
+    $nmmskpd = $this->tukd_model->get_nama($kd_skpd,'nm_skpd','ms_skpd','kd_skpd');
+	if($ttd=='-'){
+		$nama='';
+		$pangkat='';
+		$jabatan='';
+		$nip='';
+	}else{
+	$ttd=str_replace("abc"," ",$ttd);
+	//echo $ttd;
+	//$sqlsc="SELECT nama,jabatan,pangkat FROM ms_ttd where nip='$ttd' AND kode='KD'";
+	$sqlsc="SELECT TOP 1 nama as nm,nip as nip,jabatan as jab,pangkat FROM ms_ttd where kode='BUD' and nip='$ttd'";
+                 $sqlsclient=$this->db->query($sqlsc);
+                 foreach ($sqlsclient->result() as $rowttd)
+                {
+                    $nama = $rowttd->nm;
+                    $jabatan = $rowttd->jab;
+                    $pangkat = $rowttd->pangkat;
+                    $nip = 'NIP. '.$ttd;
+                } 
+	}
+		$where = "LEN(kd_rek)<='$jenis'";
+		/* $cRet ='<TABLE style="border-collapse:collapse;font-size:11px;font-family:Arial" width="100%" border="0" cellspacing="0" cellpadding="1" align=center>
+					<TR>
+						<TD  width="60%" valign="top" align="right" > &nbsp;</TD>
+						<TD width="40%"  align="left" >Lampiran II.A.1<br>Peraturan Menteri Keuangan No. 04/PMK.07/2011<br>Tentang Tata Cara Penyampaian Informasi<br>Keuangan Daerah </TD>
+					</TR>
+					<tr>
+					</TABLE><br/>';*/
+	$cRet ='';
+	$cRet .="<TABLE style=\"border-collapse:collapse;font-family:Arial\" width=\"100%\" border=\"1\" cellspacing=\"0\" cellpadding=\"1\" align=\"center\">
+					<tr>					
+					<td align=\"center\" style=\"border-bottom:hidden\"><strong>PEMERINTAH KOTA PONTIANAK </strong></td></tr>
+                    <tr><td align=\"center\" style=\"border-bottom:hidden;border-top:hidden\"><b>LAPORAN REALISASI ANGGARAN PENDAPATAN DIKAS DAERAH <br/>".strtoupper($nmmskpd)." </b></tr>
+                    <tr><td align=\"center\" style=\"border-bottom:hidden;border-top:hidden\"><b>TAHUN ANGGARAN $lntahunang </b></tr>
+					<tr><td align=\"center\" style=\"border-top:hidden\" ><b>REALISASI S/D $judul $lntahunang</b></tr>
+					</TABLE>";
+			
+		$cRet .="<table style=\"border-collapse:collapse;font-family:Arial;font-size:11px\" width=\"100%\" align=\"center\" border=\"1\" cellspacing=\"3\" cellpadding=\"3\">
+                <thead>
+				<tr>
+                    <td rowspan=\"2\" width=\"7%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>KD REK</b></td>
+                    <td rowspan=\"2\" width=\"35%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>URAIAN</b></td>
+                    <td rowspan=\"2\" width=\"15%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>ANGGARAN</b></td>
+                    <td colspan=\"3\" width=\"45%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>Realisasi (Rp.)</b></td>
+                    <td rowspan=\"2\" width=\"15%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>SISA</b></td>
+                    <td rowspan=\"2\" width=\"5%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>Persen<br>tase</b></td>
+                    <td rowspan=\"2\" width=\"5%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>DASAR HUKUM</b></td>
+				</tr>
+				<tr>
+					<td width=\"15%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>s/d Bulan Lalu</b></td>
+					<td width=\"15%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>Bulan Ini</b></td>
+					<td width=\"15%\" align=\"center\" bgcolor=\"#CCCCCC\" ><b>s/d Bulan Ini</b></td>
+					</tr>
+					<tr>
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >1</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >2</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >3</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >4</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >5</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >6=(4+5)</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" >7</td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" ></td> 
+                   <td align=\"center\" bgcolor=\"#CCCCCC\" ></td> 
+				</tr>
+				</thead>";
+				
+				$tot_nil_ang=0;
+				$tot_bulan_lalu=0;
+				$tot_bulan_ini=0;
+				$tot_sd_bulan_ini=0;
+				$sql="SELECT kd_rek,nm_rek,anggaran,bulan_lalu,bulan_ini,sd_bulan_ini FROM penerimaan_kasda_unit($bulan,$anggaran) 
+				WHERE LEFT(kd_rek,1) in ('4','6') AND left(kd_skpd,7)=left('$kd_skpd',7) AND $where 
+				ORDER BY kd_rek";
+				$hasil = $this->db->query($sql);
+                    foreach ($hasil->result() as $row)
+                    {
+					   $kd_rek = $row->kd_rek;
+                       $nm_rek = $row->nm_rek;
+                       $nil_ang = $row->anggaran;
+                       $bulan_lalu = $row->bulan_lalu;
+                       $bulan_ini = $row->bulan_ini;
+                       $sd_bulan_ini = $row->sd_bulan_ini;
+					   $sisa=$nil_ang-$sd_bulan_ini;
+					   $persen = empty($nil_ang) || $nil_ang == 0 ? 0 :$sd_bulan_ini/$nil_ang;
+					   $persen_2 = $persen*100; 
+					   $sisa1 = $sisa<0 ? $sisa*-1 :$sisa;
+					   $a = $sisa<0 ? '(' :'';
+					   $b = $sisa<0 ? ')' :'';
+
+					   
+					   $leng=strlen($kd_rek);
+					   switch ($leng) {
+					  case 1:
+					   
+					   $cRet .='<tr>
+							   <td align="left" valign="top"><b>'.$this->dotrek($kd_rek).'</b></td> 
+							   <td align="left"  valign="top"><b>'.$nm_rek.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($nil_ang, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_lalu, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($sd_bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.$a.' '.number_format($sisa1, "2", ",", ".").' '.$b.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($persen_2, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b></td> 
+							</tr>';
+						break;
+						case 2:
+						$cRet .='<tr>
+							   <td align="left" valign="top"><b>'.$this->dotrek($kd_rek).'</b></td> 
+							   <td align="left"  valign="top"><b>&nbsp;&nbsp;'.$nm_rek.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($nil_ang, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_lalu, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($sd_bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.$a.' '.number_format($sisa1, "2", ",", ".").' '.$b.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($persen_2, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b></td> 
+							</tr>';
+						break;
+						case 3:
+						$cRet .='<tr>
+							   <td align="left" valign="top"><b>'.$this->dotrek($kd_rek).'</b></td> 
+							   <td align="left"  valign="top"><b>&nbsp;&nbsp;&nbsp;'.$nm_rek.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($nil_ang, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_lalu, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($sd_bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.$a.' '.number_format($sisa1, "2", ",", ".").' '.$b.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($persen_2, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b></td> 
+							</tr>';
+						break;
+						case 5:
+						$cRet .='<tr>
+							   <td align="left" valign="top"><b>'.$this->dotrek($kd_rek).'</b></td> 
+							   <td align="left"  valign="top"><b>&nbsp;&nbsp;&nbsp;&nbsp;'.$nm_rek.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($nil_ang, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_lalu, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($sd_bulan_ini, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b>'.$a.' '.number_format($sisa1, "2", ",", ".").' '.$b.'</b></td> 
+							   <td align="right" valign="top"><b>'.number_format($persen_2, "2", ",", ".").'</b></td> 
+							   <td align="right" valign="top"><b></td> 
+							</tr>';
+						break;
+						case 7:
+						$cRet .='<tr>
+							   <td align="left" valign="top">'.$this->dotrek($kd_rek).'</td> 
+							   <td align="left"  valign="top">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$nm_rek.'</td> 
+							   <td align="right" valign="top">'.number_format($nil_ang, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.number_format($bulan_lalu, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.number_format($bulan_ini, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.number_format($sd_bulan_ini, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.$a.' '.number_format($sisa1, "2", ",", ".").' '.$b.'</td> 
+							   <td align="right" valign="top">'.number_format($persen_2, "2", ",", ".").'</td> 
+							   <td align="right" valign="top"></td> 
+							</tr>';
+						break;
+						default:
+						$cRet .='<tr>
+							   <td align="left" valign="top">&nbsp;&nbsp;&nbsp;&nbsp;'.$this->right($kd_rek,10).'</td> 
+							   <td align="left"  valign="top">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'.$nm_rek.'</td> 
+							   <td align="right" valign="top">'.number_format($nil_ang, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.number_format($bulan_lalu, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.number_format($bulan_ini, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.number_format($sd_bulan_ini, "2", ",", ".").'</td> 
+							   <td align="right" valign="top">'.$a.' '.number_format($sisa1, "2", ",", ".").' '.$b.'</td> 
+							   <td align="right" valign="top">'.number_format($persen_2, "2", ",", ".").'</td> 
+							   <td align="right" valign="top"></td> 
+							</tr>';
+						break;
+					   }
+					}
+		$sql2="SELECT sum(anggaran) as anggaran,sum(bulan_lalu) as bulan_lalu,sum(bulan_ini) as bulan_ini,sum(sd_bulan_ini) as sd_bulan_ini FROM penerimaan_kasda_unit($bulan,$anggaran) 
+				WHERE LEFT(kd_rek,1) in ('4','6') AND left(kd_skpd,7)=left('$kd_skpd',7) AND LEN(kd_rek)='2'"; 
+                $hasil = $this->db->query($sql2);
+                    foreach ($hasil->result() as $row)
+                    {
+					   $tot_nil_ang=$row->anggaran;
+					   $tot_bulan_ini=$row->bulan_ini;
+					   $tot_bulan_lalu=$row->bulan_lalu;
+					   $tot_sd_bulan_ini=$tot_bulan_ini+$tot_bulan_lalu;
+					   $tot_sisa=$tot_nil_ang-$tot_sd_bulan_ini;
+					   if($tot_sd_bulan_ini==0){
+					   	$tot_persen=0;
+					   }else{
+					   	$tot_persen=$tot_sd_bulan_ini/$tot_nil_ang;
+					   }
+					   
+					   $tot_persen_2 = $tot_persen*100;
+                     
+			$cRet .='<tr>
+				   <td colspan="2" align="center"  valign="top"><b>JUMLAH PENDAPATAN</b></td> 
+				   <td align="right" valign="top"><b>'.number_format($tot_nil_ang, "2", ",", ".").'</b></td> 
+				   <td align="right" valign="top"><b>'.number_format($tot_bulan_lalu, "2", ",", ".").'</b></td> 
+				   <td align="right" valign="top"><b>'.number_format($tot_bulan_ini, "2", ",", ".").'</b></td> 
+				   <td align="right" valign="top"><b>'.number_format($tot_sd_bulan_ini, "2", ",", ".").'</b></td> 
+				   <td align="right" valign="top"><b>'.number_format($tot_sisa, "2", ",", ".").'</b></td> 
+				   <td align="right" valign="top"><b>'.number_format($tot_persen_2, "2", ",", ".").'</b></td> 
+				   <td align="right" valign="top"><b></td> 
+				</tr>';	
+                }
+			$cRet .="</table>";
+			$cRet .="<table style=\"border-collapse:collapse;font-family:Arial;font-size:12px\" width=\"100%\" align=\"center\" border=\"0\" cellspacing=\"1\" cellpadding=\"1\">
+				<tr>
+                    <td width=\"50%\" align=\"center\">&nbsp;</td>
+                    <td width=\"50%\" align=\"center\"></td>
+				</tr>
+				<tr>
+                    <td width=\"50%\" align=\"center\">&nbsp;</td>
+                    <td width=\"50%\" align=\"center\">$tanggal<br>$jabatan<br>$pangkat<br><br><br><br><br><b><u>$nama</u></b><br>$nip
+					</td>
+				</tr>
+				</table>";
+			
+			$data['prev']= $cRet;    
+            $judul='REALISASI_PENDPATAN ';
+			switch ($ctk){
+				case 0;
+				echo ("<title>$judul</title>");
+				echo $cRet;
+				break;
+				case 1;
+				$this->tukd_model->_mpdf('',$cRet,10,10,10,'L');
+				break;
+				case 2;        
+				header("Cache-Control: no-cache, no-store, must-revalidate");
+				header("Content-Type: application/vnd.ms-excel");
+				header("Content-Disposition: attachment; filename= $judul.xls");
+				$this->load->view('anggaran/rka/perkadaII', $data);
+				break;	
+			}
+		}
 
 }
